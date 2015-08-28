f934fe5c6ae3fba00fe4a3e89a13cfc44a8ee7e6
erm0l0v <erm0l0v@ya.ru>
2015-08-28 19:58:54 +0300

    add CACHES

--- runtests.py
+++ runtests.py
@@ -1,11 +1,28 @@
 import sys
-import os
 
 try:
     from django.conf import settings
     from django.test.utils import get_runner
 
-    settings.configure()
+    settings.configure(
+        DEBUG=True,
+        USE_TZ=True,
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+            }
+        },
+        ROOT_URLCONF="django_fake_model.urls",
+        INSTALLED_APPS=[
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "django.contrib.sites",
+            "django_fake_model",
+        ],
+        SITE_ID=1,
+        MIDDLEWARE_CLASSES=(),
+        CACHES={},
+    )
 
     try:
         import django
