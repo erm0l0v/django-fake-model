4951ac7dd885f584a3698e8aa5c4a7345f361674
erm0l0v <erm0l0v@ya.ru>
2015-08-28 22:01:27 +0300

    clean settings

--- runtests.py
+++ runtests.py
@@ -5,27 +5,14 @@ try:
     from django.test.utils import get_runner
 
     settings.configure(
-        DEBUG=True,
-        USE_TZ=True,
         DATABASES={
             "default": {
                 "ENGINE": "django.db.backends.sqlite3",
             }
         },
-        ROOT_URLCONF="django_fake_model.urls",
         INSTALLED_APPS=[
-            "django.contrib.auth",
-            "django.contrib.contenttypes",
-            "django.contrib.sites",
             "django_fake_model",
         ],
-        SITE_ID=1,
-        MIDDLEWARE_CLASSES=(),
-        CACHES={
-            'default': {
-                'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-            }
-        },
     )
 
     try:
