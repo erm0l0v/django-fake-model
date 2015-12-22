eb8218de72d1789b9e054e2ee76c51558cc1a653
Saul Shanabrook <s.shanabrook@gmail.com>
2015-12-22 13:38:36 -0500

    Fix ordering of deletions on MySQL

--- django_fake_model/case_extension.py
+++ django_fake_model/case_extension.py
@@ -15,11 +15,15 @@ class CaseExtension(SimpleTestCase):
         self._map_models('create_table')
 
     def _post_teardown(self):
-        self._map_models('delete_table')
+        # If we don't remove them in reverse order, then if we created table A
+        # after table B and it has a foreignkey to table B, then trying to
+        # remove B first will fail on some configurations, as documented
+        # in issue #1
+        self._map_models('delete_table', reverse=True)
         super(CaseExtension, self)._post_teardown()
 
-    def _map_models(self, method_name):
-        for model in self._models:
+    def _map_models(self, method_name, reverse=False):
+        for model in (reversed(self._models) if reverse else self._models):
             try:
                 getattr(model, method_name)()
             except AttributeError:
